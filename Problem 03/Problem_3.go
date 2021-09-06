ecffb35af7080c3baac61447debd9d49850ca9d8
trannam886 <85615795+tiennam886@users.noreply.github.com>
2021-09-06 14:23:40 +0700

    Add files via upload

---
+++ Problem 03/Problem_3.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var nestedSize int
+	fmt.Scanln(&nestedSize)
+	nestedSlice := initMatrix(nestedSize)
+	fmt.Println(Solve(nestedSlice, nestedSize))
+}
+
+func initMatrix(nestedSize int) [][]int{
+	var nestedSlice [][]int
+	for i:=0;i<nestedSize; i++ {
+		var jSlice []int
+		for j:=0;j<nestedSize; j++ {
+			jSlice = append(jSlice, 1)
+		}
+		nestedSlice = append(nestedSlice, jSlice)
+	}
+	return nestedSlice
+}
+func Solve(nestedSlice [][]int, nestedSize int) int{
+	for i:=1;i<nestedSize;i++ {
+		for j:=1;j<nestedSize;j++{
+			nestedSlice[i][j] =nestedSlice[i][j-1]+nestedSlice[i-1][j]
+		}
+	}
+	return nestedSlice[nestedSize-1][nestedSize-1]
+}
