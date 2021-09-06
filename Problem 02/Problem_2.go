0ece3fa60d13410390ab07e1e41ebd96d61b8ef9
trannam886 <85615795+tiennam886@users.noreply.github.com>
2021-09-06 11:24:55 +0700

    Add files via upload

---
+++ Problem 02/Problem_2.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+// n is number of employees
+// l is number of leaders
+// then n-l is number of non-leader employees
+// hence, n-l is divisible by l
+
+func main(){
+	var num int
+	fmt.Scanln(&num)
+	fmt.Println(checkDivisible(num))
+
+}
+func checkDivisible(num int) int{
+	way := 0
+	var leader int
+	for leader=1; leader<=num/2; leader++ {
+		if (num-leader)%leader ==0 {
+			way +=1
+		}
+	}
+	return way
+}
