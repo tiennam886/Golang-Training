bace56398eb8acde496f1ef2a6843fe8d5bdd73c
trannam886 <85615795+tiennam886@users.noreply.github.com>
2021-09-08 11:13:12 +0700

    Update Problem_7.go

--- Problem 07/Problem_7.go
+++ Problem 07/Problem_7.go
@@ -13,9 +13,8 @@ var (
 	n     int
 	m     int
 	nShop [N]int
-
-	r = bufio.NewReader(os.Stdin)
-	w = bufio.NewWriter(os.Stdout)
+	r     = bufio.NewReader(os.Stdin)
+	w     = bufio.NewWriter(os.Stdout)
 )
 
 func main() {
@@ -23,32 +22,35 @@ func main() {
 	solved()
 }
 func solved() {
-	_, _ = fmt.Fscan(r, &n)
+	fmt.Fscan(r, &n)
 	for i := 0; i < n; i++ {
 		var x int
-		_, _ = fmt.Fscan(r, &x)
+		fmt.Fscan(r, &x)
 		nShop[i] = x
 	}
 	sort.Ints(nShop[0:n])
 	// sort min->max
-	_, _ = fmt.Fscan(r, &m)
+	fmt.Fscan(r, &m)
 	for i := 0; i < m; i++ {
 		var x int
-		_, _ = fmt.Fscan(r, &x)
+		fmt.Fscan(r, &x)
 		l := 0
 		r := n
-		var mid int
-		for l < r {
-			mid = (l + r) / 2
-			if nShop[mid] <= x {
-				l = mid + 1
-			} else {
-				r = mid
-			}
-		}
+
 		// find the minimum position of x that x >= sorted_nShop[i]
-		fmt.Println(l)
+		fmt.Println(upperbound(l, r, x, nShop[0:n]))
 
 	}
 }
-
+func upperbound(l int, r int, x int, nShop []int) int {
+	var mid int
+	for l < r {
+		mid = (l + r) / 2
+		if nShop[mid] <= x {
+			l = mid + 1
+		} else {
+			r = mid
+		}
+	}
+	return l
+}
