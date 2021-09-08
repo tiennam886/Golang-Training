e8683036513e87069bab1311ac9d6acd16b4559b
trannam886 <85615795+tiennam886@users.noreply.github.com>
2021-09-08 10:57:48 +0700

    Update Problem_6.go

--- Problem 06/Problem_6.go
+++ Problem 06/Problem_6.go
@@ -2,45 +2,44 @@ package main
 
 import (
 	"bufio"
-	"os"
 	"fmt"
+	"os"
 )
+
 var (
-	ans int64 
-	n int
-	m int
+	ans  int64
+	n    int
+	m    int
 	save = make([]int, 3)
-
-	r = bufio.NewReader(os.Stdin)
-	w = bufio.NewWriter(os.Stdout)
+	r    = bufio.NewReader(os.Stdin)
+	w    = bufio.NewWriter(os.Stdout)
 )
 
-
-func solve(){
-	save[0]=0 // save even num
-	save[1]=0 // save odd num
+func solve() {
+	save[0] = 0 // save even num
+	save[1] = 0 // save odd num
 	ans = 0
-	_, _ = fmt.Fscan(r, &n)
-	for i:=1;i<=n;i++{
-		var x int 
-		_, _ = fmt.Fscan(r, &x)
+	fmt.Fscan(r, &n)
+	for i := 1; i <= n; i++ {
+		var x int
+		fmt.Fscan(r, &x)
 		save[x%2]++
 		// count even and odd nums in array
 	}
-	_, _ = fmt.Fscan(r, &m)
-	for i:=1;i<=m;i++{
-		var x int 
-		_, _ = fmt.Fscan(r, &x)
+	fmt.Fscan(r, &m)
+	for i := 1; i <= m; i++ {
+		var x int
+		fmt.Fscan(r, &x)
 		ans += (int64(save[x%2]))
-		// ans = number of even pair and odd pair
+		// ans is number of even pair and odd pair
 	}
-	_, _ = fmt.Println(int64(ans))
+	fmt.Println(int64(ans))
 }
-func main(){
+func main() {
 	defer w.Flush()
-	var te int 
-	_, _ = fmt.Fscan(r, &te)
-	for i := 1; i <= te; i++{
+	var te int
+	fmt.Fscan(r, &te)
+	for i := 1; i <= te; i++ {
 		solve()
 	}
 }
