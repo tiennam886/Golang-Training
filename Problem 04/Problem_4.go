3f46a69daddefd3fef73aed7886fb0d4ddfd861d
trannam886 <85615795+tiennam886@users.noreply.github.com>
2021-09-08 10:54:26 +0700

    Update Problem_4.go

--- Problem 04/Problem_4.go
+++ Problem 04/Problem_4.go
@@ -2,37 +2,28 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
 
 // Assume c is the largest num, then it must be c<=b+a-1 to make a triangle
 // So if a,b,c do not make a triangle, the minimum num must be added is c+1-b-a
-func main()  {
-	var a int
-	var b int
-	var c int
-	_, err := fmt.Scanf("%d %d %d", &a,&b, &c)
-	if err != nil {
-		return 
-	}
-	a,b,c = findMax(a,b,c)
-	fmt.Println(solve(a,b,c))
+
+var (
+	a int
+	b int
+	c int
+)
+
+func main() {
+	fmt.Scanf("%d %d %d", &a, &b, &c)
+	arr := [3]int{a, b, c}
+	sort.Ints(arr[:])
+	fmt.Println(solve(arr[0], arr[1], arr[2]))
 }
-func findMax(a int, b int, c int) (int, int, int){
-	if a>=b && a>=c{
-		return a, b, c
+func solve(a int, b int, c int) int {
+	if c >= (a + b) {
+		return c + 1 - b - a
 	}
-	if b>=c && b>=a{
-		return b, a, c
-	}
-	if c>=b && c>=a{
-		return c,b,a
-	}
-	return 0,0,0
-}
-func solve(a int, b int, c int) int{
-	if a >= (b+c) {
-		return a+1-b-c
-	}else{
-		return 0
-	}
+	return 0
+
 }
