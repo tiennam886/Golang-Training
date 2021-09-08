455c812e03d7fc70f215b96260f48cc32bc13b96
trannam886 <85615795+tiennam886@users.noreply.github.com>
2021-09-08 11:15:13 +0700

    Update Problem_8.go

--- Problem 08/Problem_8.go
+++ Problem 08/Problem_8.go
@@ -18,14 +18,14 @@ func main() {
 	defer w.Flush()
 	const N int = 1e5 + 10
 
-	_, _ = fmt.Fscan(r, &n)
+	fmt.Fscan(r, &n)
 	if n < 2 {
 		return
 	}
 	var queues [N]int
 	for i := 1; i <= n; i++ {
 		var k int
-		_, _ = fmt.Fscan(r, &k)
+		fmt.Fscan(r, &k)
 		queues[i] = k
 	}
 	for i := 1; i <= n; i++ {
@@ -36,7 +36,8 @@ func main() {
 
 		}
 	}
-    // count and find the minimum rounds that a queue can be 0, prior to initial queue
+	// count and find the minimum rounds that a queue can be 0, prior to head-to-tail queue
 	fmt.Println(ans)
 
 }
+
