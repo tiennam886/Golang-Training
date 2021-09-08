a09e30a31595f97bc868bd1404af27659035c46e
trannam886 <85615795+tiennam886@users.noreply.github.com>
2021-09-08 10:56:09 +0700

    Update Problem_5.go

--- Problem 05/Problem_5.go
+++ Problem 05/Problem_5.go
@@ -7,22 +7,13 @@ import (
 
 func main(){
 	var n int
-	_, err := fmt.Scanln(&n)
-	if err != nil {
-		fmt.Println("1")
-	}
+	fmt.Scanln(&n)
 	var result string
 	for i:=0;i<n;i++{
 		var length int
-		_, err2 := fmt.Scanln(&length)
-		if err2 != nil {
-			fmt.Println("2")
-		}
+		fmt.Scanln(&length)
 		var phone string
-		_, err3 := fmt.Scanln(&phone)
-		if err3 != nil {
-			fmt.Println("3")
-		}
+		fmt.Scanln(&phone)
 		result += "\n"+checkPhoneNum(length,phone)
 	}
 	fmt.Println(result)
@@ -32,7 +23,5 @@ func checkPhoneNum(length int, phone string) string{
 	if first8Index!=-1 && (length-first8Index) >=11{
 		return "YES"
 	}
-
 	return "NO"
 }
-
